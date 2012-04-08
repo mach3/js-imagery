f29bf440c3a149d12ad9a9506abedf2181ebc09f
mach3 <mach3ss@gmail.com>
2012-04-08 22:59:42 +0900

    initialize

---
+++ js/imagery.js
@@ -0,0 +1,230 @@
+(function($, undefined){
+
+	var Imagery = ({
+		type : "imagery",
+		png : true,
+		option : {
+			events : [ "click", "mouseover", "mouseout", "mouseenter", "mouseleave" ]
+		},
+		initialize:function(){
+			var ua = navigator.userAgent.match(/MSIE\s([\d\.]+?);/);
+			if(ua !== null){
+				this.png = parseInt(ua[1]) > 8;
+			}
+			if(! this.png){
+				this.initVml();
+			}
+			return this;
+		},
+		initVml : function(){
+			var self = this;
+			// add namespace
+			(function(ns){
+				if(!ns[self.type]){
+					ns.add(self.type, "urn:schemas-microsoft-com:vml");
+				}
+			}(document.namespaces));
+			// add selector
+			(function(){
+				var ss, style;
+				ss = $("<style>").appendTo($("head"))[0].styleSheet;
+				style = "{behavior:url(#default#VML);display:inline-block;}";
+				ss.addRule(self.type + "\\:rect", style);
+				ss.addRule(self.type + "\\:fill", style);
+			}());
+			return this;
+		},
+		createVmlImage : function(src, width, height){
+			var self, rect, fill;
+			self = this;
+			rect = $(document.createElement(self.type + ":rect"));
+			rect.prop("stroked", false);
+			rect.css({
+				position:"absolute",
+				width:width,
+				height:height
+			});
+			fill = $(document.createElement(self.type + ":fill"));
+			fill.prop({
+				src : src,
+				on : true,
+				type : "frame"
+			});
+			fill.appendTo(rect);
+			rect.on("append", function(){
+				$(this).find("fill")[0].color = "none";
+			});
+			return rect;
+		},
+
+		simplate : function(str, vars){
+			return str.replace(/{{(\w+?)}}/g, function(a,b){
+				return vars[b] || "";
+			});
+		},
+		isLoaded : function(img){
+			if(img.tagName.toLowerCase() != "img"){ return null; }
+			if(! img.complete){ return false; }
+			if(typeof img.naturalWidth != "undefined" && img.naturalWidth == 0){ return false; }
+			return true;
+		},
+		isImg : function(ele){
+			return ele.nodeName.toLowerCase() == "img";
+		},
+		getVml : function(ele){
+			var vml = $(ele).data(this.type + "-vml");
+			if(vml){
+				return vml;
+			}
+			return null;
+		},
+		getSource : function(ele){
+			var o = $(ele);
+			if(this.isImg(ele)){
+				return $(ele).prop("src");
+			} else {
+				var m = o.css("background-image").match(/url\("?(.+?)"?\)$/);
+				return m[1] || "";
+			}
+			return null;
+		},
+		getStateSource : function(src, postfix){
+			return src.replace(/\.(\w+?)$/, function(a,b){
+				return postfix + a;
+			});
+		},
+
+
+		/**
+		 * Public
+		 */
+		alpha : function($ele){
+			var self, set;
+			if(this.png){ return; }
+			self = this;
+			set = function(){
+				var o, src, vml;
+				o = $(this);
+				src = self.getSource(this);
+				if(/\.png$/.test(src)){
+					vml = self.createVmlImage(
+						self.getSource(this),
+						o.width(),
+						o.height()
+					);
+					vml.insertBefore(o).trigger("append");
+					o.css("visibility", "hidden");
+					o.data(self.type + "-vml", vml);
+					vml.on(self.option.events.join(" "), function(e){
+						// todo : why called twice ?
+						o.trigger(e.type);
+					});
+				}
+			};
+			$ele.each(function(){
+				var o;
+				o = $(this);
+				if(this.tagName.toLowerCase() == "img" && !o.width()){
+					o.on("load", set);
+				} else {
+					set.call(this);
+				}
+			});
+		},
+		swap : function($ele, src){
+			var ele, vml;
+			ele = $ele[0];
+			vml = this.getVml(ele);
+			if(vml){
+				vml.find("fill").prop("src", src);
+				return;
+			}
+			if(this.isImg(ele)){
+				$ele.attr("src", src);
+			} else {
+				$ele.css("background-image", "url(" + src + ")");
+			}
+		},
+		swapOnHover : function($ele, config){
+			var self, option, handler;
+
+			self = this;
+			option = {
+				postfix : "-hover",
+				normal : this.type + "-default-src",
+				hover : this.type + "-hover-src",
+				ignore : function(){ return true; }
+			};
+			$.extend(option, config);
+			handler = function(e){
+				var o = $(e.target);
+				if(! option.ignore.call(this)){
+					switch(e.type){
+						case "mouseenter" : self.swap(o, o.data(option.hover)); break;
+						case "mouseleave" : self.swap(o, o.data(option.normal)); break;
+						default : break;
+					}
+				}
+			};
+
+			$ele.each(function(){
+				var ele, o, src, hover;
+				ele = this;
+				o = $(this);
+				src = self.getSource(this);
+				o.data(option.normal, src);
+				o.data(option.hover, self.getStateSource(src, option.postfix));
+				hover = new Image();
+				hover.onload = function(){
+					o.hover(handler, handler);
+				};
+				hover.src = o.data(option.hover);
+			});
+		},
+		fade : function($ele, opacity, config){
+		}
+	}).initialize();
+
+	window.Imagery = Imagery;
+
+
+
+	var foo = $(".foo");
+	foo.on("click", function(){
+		alert("click");
+
+	});
+	Imagery.alpha(foo);
+	Imagery.swapOnHover(foo, {
+		ignore : function(){
+			return $(this).data("ignore") == true;
+		}
+
+	});
+
+
+	var bar = $(".bar");
+	Imagery.alpha(bar);
+	Imagery.swapOnHover(bar, {
+		ignore : function(){
+			return $(this).data("ignore") == true;
+		}
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}(jQuery));
